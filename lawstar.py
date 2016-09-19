dce4328b30a19be831f535d44961ba5a57448b5f
Moxikai <zhu-hero@qq.com>
2016-09-19 16:51:55 +0800

    add webdriver for load detailpage

--- lawstar.py
+++ lawstar.py
@@ -7,6 +7,7 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 import hashlib
+import os
 from time import sleep
 from urlparse import urljoin
 import requests
@@ -45,6 +46,41 @@ class LawStar():
                         'Accept-Encoding':'gzip, deflate, sdch',
                         'Accept-Language':'zh-CN,zh;q=0.8',
                         }
+        #缓存位置
+        self.dirpath = os.path.join(os.path.dirname(__file__),'.cache')
+        #共享浏览器驱动
+        self.driver = None
+        #解析错误,信息保存路径
+        self.errorInfoPath = os.path.join(os.path.dirname(__file__),'errorinfo.txt')
+        #每次浏览器设置后加载次数
+        self.countPerSet = 0
+        #浏览器渲染总次数
+        self.countTotal = 0
+
+    #设置浏览器驱动
+    def setDriver(self):
+
+        from selenium import webdriver
+        driverPath = os.path.join(os.path.dirname(__file__), 'chromedriver')
+        self.driver = webdriver.Chrome(executable_path=driverPath)
+
+    #通过浏览器下载详情页面
+    def downlaodByDriver(self,url):
+        if self.countPerSet > 8:
+            #计数器归零
+            self.countPerSet = 0
+            #重启驱动
+            self.driver.quit()
+            sleep(0.5)
+            self.setDriver()
+            self.downlaodByDriver(url)
+        else:
+            self.driver.get(url)
+            content = self.driver.page_source
+            self.countPerSet += 1
+            self.countTotal += 1
+            print '本次驱动加载次数%s,总加载次数%s'%(self.countPerSet,self.countTotal)
+            return content
 
     #获取列表页源码
     def getListPage(self,page=1):
@@ -62,7 +98,6 @@ class LawStar():
         #默认1页,不传递参数
         if page > 1:
             params['p']=page
-
         r = requests.get(url=self.url,headers=self.headers,params=params,)
         #print r.content
         return r.content
@@ -95,33 +130,95 @@ class LawStar():
             return False
 
 
+    #检测是否获取到详情页面实际内容
+    def checkUseable(self,content):
+        pass
+        soup = BeautifulSoup(content,'lxml')
+        if soup.find('div',class_='zhengwen').div:
+            pass
+            return True
+        else:
+            return False
+
+    #保存详情页面到本地
+    def saveHtmlCache(self,content,name):
+        #检测文件夹是否存在
+        if not os.path.exists(self.dirpath):
+            os.makedirs(self.dirpath)
+            print '新建文件夹%s'%(self.dirpath)
+        filename = os.path.join(self.dirpath,name+'.html')
+        with open(filename,'w') as f:
+            f.write(content)
+        print '文件%s保存完毕!'%(name)
+
+
+    #检测是否存在缓存
+    def checkExistCache(self,name):
+
+        filename = os.path.join(self.dirpath,name+'.html')
+        if os.path.exists(filename):
+            return filename
+        else:
+            return False
+
+
+    #从缓存文件读取内容
+    def readFromCache(self,filepath):
+
+        with open(filepath,'r') as f:
+            return f.read()
+
+    #保存出错的网页信息
+    def saveParseError(self,title,url):
+        pass
+        str = title+','+url+'\n'
+        with open(self.errorInfoPath,'a') as f:
+            f.write(str)
+            print '记录%解析错误,详情信息已保存至文件'
+
+
     #解析详情页面
     def getDetail(self,url,title):
-        content = self.getDetailPage(url,title)
-        if content:
-            tree = etree.HTML(content,etree.HTMLParser(encoding='utf-8'))
-            #解析概要信息
-            wenhao = tree.xpath('//*[@id="fgzw_right2"]/ul/li[2]/text()') #法规文号
-            wenhao = [i.split('】')[-1].strip() for i in wenhao] #清理前缀
+        #检测是否存在缓存
+        name = hashlib.sha1(url).hexdigest()
+        cache = self.checkExistCache(name)
+        if cache:
+            print '检测到缓存!'
+            content = self.readFromCache(cache)
+        else:
+            #content = self.getDetailPage(url,title)
+            content = self.downlaodByDriver(url)
+            if content and self.checkUseable(content):
+                #缓存内容至文件
+                self.saveHtmlCache(content,name)
+            else:
+                return False
 
-            publish_date = tree.xpath('//*[@id="tdat"]/text()') #发布日期
-            publish_date = [i.split('】')[-1].strip() for i in publish_date] #清理前缀
+        tree = etree.HTML(content,etree.HTMLParser(encoding='utf-8'))
+        #解析概要信息
 
-            done_date = tree.xpath('//*[@id="fgzw_right2"]/ul/li[4]/text()') #实施日期
-            done_date = [i.split('】')[-1].strip() for i in done_date]
+        wenhao = tree.xpath('//*[@id="fgzw_right2"]/ul/li[2]/text()') #法规文号
+        wenhao = [i.split('】')[-1].strip() for i in wenhao] #清理前缀
 
-            publish_department = tree.xpath('//*[@id="tdpt"]/text()') #发布部门
-            publish_department = [i.split('】')[-1].strip() for i in publish_department]
+        publish_date = tree.xpath('//*[@id="tdat"]/text()') #发布日期
+        publish_date = [i.split('】')[-1].strip() for i in publish_date] #清理前缀
 
-            law_class = tree.xpath('//*[@id="fgzw_right2"]/ul/li[6]/text()') #效力等级
-            law_class = [i.split('】')[-1].strip() for i in law_class]
+        done_date = tree.xpath('//*[@id="fgzw_right2"]/ul/li[4]/text()') #实施日期
+        done_date = [i.split('】')[-1].strip() for i in done_date]
 
-            #转成字符串
-            zhengwen = self.parseZhengwen(content)
+        publish_department = tree.xpath('//*[@id="tdpt"]/text()') #发布部门
+        publish_department = [i.split('】')[-1].strip() for i in publish_department]
 
-            #获取url签名
-            id = hashlib.sha1(url).hexdigest()
-            print 'id是%s'%(id)
+        law_class = tree.xpath('//*[@id="fgzw_right2"]/ul/li[6]/text()') #效力等级
+        law_class = [i.split('】')[-1].strip() for i in law_class]
+
+        #转成字符串
+        zhengwen = self.parseZhengwen(content)
+
+        #获取url签名
+        id = hashlib.sha1(url).hexdigest()
+        try:
+        #print 'id是%s'%(id)
             return {'title':title,
                     'id':id,
                     'url':url,
@@ -131,8 +228,11 @@ class LawStar():
                     'publish_department':publish_department[0],
                     'law_class':law_class[0],
                     'zhengwen':zhengwen}
-        else:
-            print '打开详情页面出错!'
+        except Exception as e:
+            print '解析数据出错!','\n',e
+            #记录出错的网页
+            self.saveParseError(title,url)
+            return False
 
     #解析大段正文
     def parseZhengwen(self,content):
@@ -150,7 +250,7 @@ class LawStar():
             for item in dict_:
             #解析详情数据
                 data = self.getDetail(item['url'],item['title'])
-                sleep(0.5)
+                sleep(1)
                 if data:
                 #保存
                     self.saveToSQLite(**data)
@@ -175,8 +275,8 @@ class LawStar():
             print '程序出错,请检查'
             return False
         #检测数据是否存在
-        law = session.query(Law).filter(Law.id == kwargs['id']).all()
-        if len(law) > 0:
+        law = session.query(Law).filter(Law.id == kwargs['id']).first()
+        if not law is None:
             pass
             print '数据已存在,不必重复保存'
         else:
@@ -207,8 +307,9 @@ class LawStar():
 
 
 if __name__ == '__main__':
-    pass
-    test= LawStar(titles='传销')
+
+    test= LawStar(titles='诈骗',contents='P2P')
+    test.setDriver()
     test.getDataByDepth()
 
 
